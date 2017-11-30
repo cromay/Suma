e4e06795db4841eb9fd4494275a4094bdd47d928
cromay <cromay@gmail.com>
2017-11-30 14:54:43 +0100

    Esta es mi primera versión
    Es simplemente una suma de dos variables A y B

---
+++ src/Sumando.java
@@ -0,0 +1,17 @@
+
+public class Sumando {
+
+	static public int a = 4;
+	static public int b = 6;
+	static public int c = a + b;
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("la suma de "+ a + " más "+ b +" es: " + c);
+		
+		
+		
+	}
+
+}
